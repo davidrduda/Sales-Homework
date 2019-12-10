d06e49abcaae59af959da4665f98d7e139d890c6
DudaD <DudaD@T29150.niaccit.niacc.edu>
2019-12-10 09:30:06 -0600

    smile

--- Sales-Homework/public_html/saleshomework.js
+++ Sales-Homework/public_html/saleshomework.js
@@ -1,19 +1,16 @@
 function totalSales() {
-    var sales = [document.forms["DailySales"]["sales"].value];
-    if (isNaN(sales)) {
-        alert("Please enter only numeric values.");
+    var sales = [];
+    sales[0] = document.forms["DailySales"]["mon"].value;
+    sales[1] = document.forms["DailySales"]["tue"].value;
+    sales[2] = document.forms["DailySales"]["wed"].value;
+    sales[3] = document.forms["DailySales"]["thu"].value;
+    sales[4] = document.forms["DailySales"]["fri"].value;
+    sales[5] = document.forms["DailySales"]["sat"].value;
+    sales[6] = document.forms["DailySales"]["sun"].value;
+    var total = 0;
+    for (i = 0; i < sales.length; i++){
+        total = total + Number(sales[i]);
     }
-    else {
-        var dailySales = document.forms["DailySales"]["sales"].value;
-        for (i = 0; i < 7; i++){
-            sales[i] = document.forms["DailySales"]["sales"].value;
-            sales.push(Number(document.forms["DailySales"]["sales"].value));
-        }
-        var total = 0;
-        for (i = 0; i < sales.length; i++){
-            total = total + sales[i];
-        }
-        var display = document.getElementById("TotalSales").innerHTML = "Total sales for the week is "+total+".";
-    }
+    document.getElementById("TotalSales").innerHTML = "Total sales for the week is "+total+".";
     return false;
 }
